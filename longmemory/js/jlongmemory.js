0bbdc59e7bf8fad91edd29b8f5db9b3050e4c5ec
Владислав <epifanov,vld@yandex.ru>
2016-07-13 10:27:42 +0300

    select

--- longmemory/js/jlongmemory.js
+++ longmemory/js/jlongmemory.js
@@ -1,196 +1,271 @@
-var indexedDB 	  = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB,
-IDBTransaction  = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+var indexedDB = window.indexedDB || window.mozIndexedDB
+		|| window.webkitIndexedDB || window.msIndexedDB, IDBTransaction = window.IDBTransaction
+		|| window.webkitIDBTransaction || window.msIDBTransaction;
 
-var baseName  = "filesBaselongmemory";
+var baseName = "filesBaselongmemory";
 var storeName = "";
 var bName = "baseParam";
 var sName = "storeParam";
 var baseversion = 1;
-(function()//считывание версии базы
-{
-    connectDB_Param(function(db){
-		var request = db.transaction([sName], "readonly").objectStore(sName).get(1);
+var countDeck = 0;
+var dateRepeat = new Date(2000, 0, 1, 0, 0, 0, 0);
+var counter=0;///????
+function refreshSelectDeck1() {
+	counter++;
+	connectDB_forStoreName(function(db) {
+		var request = db.transaction("storeforStoreName", "readonly")
+				.objectStore("storeforStoreName").get(counter);
 		request.onerror = logerr;
-		request.onsuccess = function(){
-                  //alert(request.result.version);
-                  baseversion=request.result.version;
+		request.onsuccess = function() {
+			console.log(request.result.name);
+			addopt(request.result.name);
+			if(counter<countDeck) refreshSelectDeck1()
+		}
+	})
+
+}
+
+function refreshSelectDeck() {
+	connectDB_forStoreName(function(db) {
+		var request = db.transaction("storeforStoreName", "readonly")
+				.objectStore("storeforStoreName").get(0);
+		request.onerror = logerr;
+		request.onsuccess = function() {
+			
+				countDeck = request.result.count;
+				refreshSelectDeck1();
+			
 		};
-                });
+	});
+	
+
+}
+(function()// считывание версии базы
+{
+	connectDB_Param(function(db) {
+		var request = db.transaction([ sName ], "readonly").objectStore(sName)
+				.get(1);
+		request.onerror = logerr;
+		request.onsuccess = function() {
+			// alert(request.result.version);
+			baseversion = request.result.version;
+		};
+	});
 })();
-function logerr(err){
+function logerr(err) {
 	console.log(err);
 }
-function connectDB_Param(f)//коннкт для базы с версией базы колод
+function connectDB_Param(f)// коннкт для базы с версией базы колод
 {
 	var request = indexedDB.open(bName, 1);
-        request.onerror = logerr;
-	request.onsuccess = function(){
+	request.onerror = logerr;
+	request.onsuccess = function() {
 		f(request.result);
-                
+
 	};
-	request.onupgradeneeded = function(e){
-		var some=e.currentTarget.result.createObjectStore(sName, { keyPath: "path" });
-                some.createIndex("by_version", "version", {unique: true});
-                some.put({path: 1, version: 1});
+	request.onupgradeneeded = function(e) {
+		var some = e.currentTarget.result.createObjectStore(sName, {
+			keyPath : "path"
+		});
+		some.createIndex("by_version", "version", {
+			unique : true
+		});
+		some.put({
+			path : 1,
+			version : 1
+		});
 		connectDB(f);
 	};
 };
-function connectDB_forStoreName(f)//для хранения storeName-ов
-{        
+function connectDB_forStoreName(f)// для хранения storeName-ов
+{
 	var request = indexedDB.open("baseforStoreName", 1);
-        request.onerror = logerr;
-	request.onsuccess = function(){
+	request.onerror = logerr;
+	request.onsuccess = function() {
 		f(request.result);
-                
+
 	};
-	request.onupgradeneeded = function(e){
-		var some=e.currentTarget.result.createObjectStore("storeforStoreName", { keyPath: "name" });
-                //some.createIndex("by_name", "name", {unique: true});
-                some.createIndex("by_count", "count", {unique: false});
-                some.put({ name: "count_", count:0});
+	request.onupgradeneeded = function(e) {
+		var some = e.currentTarget.result.createObjectStore(
+				"storeforStoreName", {
+					keyPath : "id"
+				});
+		some.createIndex("by_name", "name", {
+			unique : true
+		});
+		some.createIndex("by_count", "count", {
+			unique : false
+		});
+		some.put({
+			id : 0,
+			name : "count_",
+			count : 0
+		});
 		connectDB(f);
 	};
 };
 function connectDB(f)// для хранения колод
-{        
+{
+	var d = new Date(2001, 0, 1, 0, 0, 0, 0);
 	var request = indexedDB.open(baseName, baseversion);
-        request.onerror = logerr;
-	request.onsuccess = function(){
+	request.onerror = logerr;
+	request.onsuccess = function() {
 		f(request.result);
-                
+
 	};
-	request.onupgradeneeded = function(e){
-		var some=e.currentTarget.result.createObjectStore(storeName, { keyPath: "path" });
-                some.createIndex("by_question", "question", {unique: true});
-                some.createIndex("by_answer", "answer");
-                some.put({path: 0, question: "how many", answer: 0});
-		
-                
+	request.onupgradeneeded = function(e) {
+		var some = e.currentTarget.result.createObjectStore(storeName, {
+			keyPath : "path"
+		});
+		some.createIndex("by_question", "question", {
+			unique : true
+		});
+		some.createIndex("by_answer", "answer");
+		some.createIndex("by_repeat", "repeat");
+		some.put({
+			path : 0,
+			question : "how many",
+			answer : 0,
+			repeat : d
+		});
+
 		connectDB(f);
 	};
 };
-function addopt()//добавление в список опций на главной странице
+function addopt(nameOption)// добавление в список опций на главной странице
 {
-    var select= document.getElementById('s1');
-    var newOption = new Option(storeName, storeName);
-    select.appendChild(newOption);
-    newOption.selected = true;
+	var select = document.getElementById('s1');
+	var newOption = new Option(nameOption, nameOption);
+	select.appendChild(newOption);
+	newOption.selected = true;
 }
-function add_version_for_db()//добавление версии(увеличение на 1)
+function add_version_for_db()// добавление версии(увеличение на 1)
 {
-            
-        connectDB_Param(function(db){
-		var request = db.transaction([sName], "readonly").objectStore(sName).get(1);
-		request.onerror = logerr;
-		request.onsuccess = function(){
-                  alert(request.result.version);
-                  baseversion=request.result.version;
-		};
-                (function ()
-                    {
-                        connectDB_Param(function(db){
 
-                                    var fileParam={
-                                        path:1,
-                                        version: baseversion+1    
-                                    };
-                                    var request = db.transaction([sName], "readwrite").objectStore(sName).put(fileParam);
-                                    request.onerror = logerr;
-                                    request.onsuccess = function(){
-                                            return request.result;
-
-                                    };
-                            });
-                    })();      
-        });
-}
-function add_deck()//добавлении колоды
-{
-    (function(){alert(document.getElementById("createpage-kol-name").value);
-        storeName="store"+document.getElementById("createpage-kol-name").value;
-        add_version_for_db();
-        (function()//добавление хранилища в бд
-        {
-        	console.log(storeName);
-            connectDB(function(db){
-            		console.log(storeName);
-                    var request = db.transaction([storeName], "readwrite").objectStore(storeName).get("");
-                    request.onerror = logerr;
-                    request.onsuccess = function(){
-                            return request.result;
-                    };
-            });
-        })();
-    })();
-        
-    var fileForStoreName={
-            name: storeName,
-            count:0
-        };
-    (function()// получение count
-    {
-        connectDB_forStoreName(function(db){
-		var request = db.transaction("storeforStoreName", "readonly").objectStore("storeforStoreName").get("count_");
+	connectDB_Param(function(db) {
+		var request = db.transaction([ sName ], "readonly").objectStore(sName)
+				.get(1);
 		request.onerror = logerr;
-		request.onsuccess = function(){
-                        
-                        fileForStoreName.count=request.result.count;
-                        console.log(fileForStoreName.count);
-                        (function()//добавление имени колоды в storeforStoreName
-                        {
-                            fileForStoreName.count=fileForStoreName.count+1;
-                            console.log(fileForStoreName.count);
-                            fileForStoreName.name=storeName;
-                            connectDB_forStoreName(function(db){
-                                var request = db.transaction("storeforStoreName", "readwrite").objectStore("storeforStoreName").put(fileForStoreName);
-                                request.onerror = logerr;
-                                request.onsuccess = function(){
-                                    return request.result;      
-                                };
-                                    (function()// увеличение count
-                                    {
-                                        console.log(fileForStoreName.count);
-                                        fileForStoreName.name="count_";
-                                        connectDB_forStoreName(function(db){
-                                                var request = db.transaction("storeforStoreName", "readwrite").objectStore("storeforStoreName").put(fileForStoreName);
-                                                request.onerror = logerr;
-                                                request.onsuccess = function(){
-                                                        return request.result;
-                                                };
-                                        });
-                                    })();
-                            });
-                        })();
+		request.onsuccess = function() {
+			alert(request.result.version);
+			baseversion = request.result.version;
 		};
-                    
+		(function() {
+			connectDB_Param(function(db) {
+
+				var fileParam = {
+					path : 1,
+					version : baseversion + 1
+				};
+				var request = db.transaction([ sName ], "readwrite")
+						.objectStore(sName).put(fileParam);
+				request.onerror = logerr;
+				request.onsuccess = function() {
+					return request.result;
+
+				};
+			});
+		})();
 	});
-        
-    })();
+}
+function add_deck()// добавлении колоды
+{
+	(function() {
+		alert(document.getElementById("createpage-kol-name").value);
+		storeName = "store"
+				+ document.getElementById("createpage-kol-name").value;
+		add_version_for_db();
+		(function()// добавление хранилища в бд
+		{
+			console.log(storeName);
+			connectDB(function(db) {
+				console.log(storeName);
+				var request = db.transaction([ storeName ], "readwrite")
+						.objectStore(storeName).get("");
+				request.onerror = logerr;
+				request.onsuccess = function() {
+					return request.result;
+				};
+			});
+		})();
+	})();
 
-    
-    
+	var fileForStoreName = {
+		id : 1,
+		name : storeName,
+		count : 0
+	};
+	(function()// получение count
+	{
+		connectDB_forStoreName(function(db) {
+			var request = db.transaction("storeforStoreName", "readonly")
+					.objectStore("storeforStoreName").get(0);
+			request.onerror = logerr;
+			request.onsuccess = function() {
+
+				fileForStoreName.count = request.result.count;
+				console.log(fileForStoreName.count);
+				(function()// добавление имени колоды в storeforStoreName
+				{
+					fileForStoreName.count = fileForStoreName.count + 1;
+					console.log(fileForStoreName.count);
+					fileForStoreName.name = storeName;
+					fileForStoreName.id = fileForStoreName.count;
+
+					connectDB_forStoreName(function(db) {
+						var request = db.transaction("storeforStoreName",
+								"readwrite").objectStore("storeforStoreName")
+								.put(fileForStoreName);
+						request.onerror = logerr;
+						request.onsuccess = function() {
+							return request.result;
+						};
+						(function()// увеличение count
+						{
+							console.log(fileForStoreName.count);
+							fileForStoreName.name = "count_";
+							fileForStoreName.id = 0;
+							connectDB_forStoreName(function(db) {
+								var request = db.transaction(
+										"storeforStoreName", "readwrite")
+										.objectStore("storeforStoreName").put(
+												fileForStoreName);
+								request.onerror = logerr;
+								request.onsuccess = function() {
+									return request.result;
+								};
+							});
+						})();
+					});
+				})();
+			};
+
+		});
+
+	})();
+
 }
 
+// function getFile(file){
+// connectDB(function(db){
+// var request = db.transaction([storeName],
+// "readonly").objectStore(storeName).get(file);
+// request.onerror = logerr;
+// request.onsuccess = function(){
+// alert(request.result.title);
+// //f(request.result ? request.result : -1);
+// f.path=request.result.path;
+// alert(f.path);
+// f.title=request.result.title;
+// f.author=request.result.author;
+// };
+// });
+// }
 
-//function getFile(file){
-//	connectDB(function(db){
-//		var request = db.transaction([storeName], "readonly").objectStore(storeName).get(file);
-//		request.onerror = logerr;
-//		request.onsuccess = function(){
-//                        alert(request.result.title);
-//			//f(request.result ? request.result : -1);
-//                        f.path=request.result.path;
-//                        alert(f.path);
-//                        f.title=request.result.title;
-//                        f.author=request.result.author;
-//		};
-//	});
-//}
-
-/*(function del(){
-    window.indexedDB.deleteDatabase("baseParam");
-    window.indexedDB.deleteDatabase("baseforStoreName");
-    window.indexedDB.deleteDatabase("filesBaselongmemory");
-    
-}());*/
-    
+/*
+ * (function del(){ window.indexedDB.deleteDatabase("baseParam");
+ * window.indexedDB.deleteDatabase("baseforStoreName");
+ * window.indexedDB.deleteDatabase("filesBaselongmemory");
+ * 
+ * }());
+ */
